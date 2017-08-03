ef23f0e49de33582186ae1b2c0515f8a74507dc6
Diabeu <scibor.rudnicki@ster-project.pl>
2017-08-03 12:30:06 +0200

    chore(gitignore): ignore dist

--- dist/index.d.ts
+++
@@ -1,2 +0,0 @@
-export * from './ngx-markdown.component';
-export * from './ngx-markdown.module';
--- dist/index.js
+++
@@ -1,3 +0,0 @@
-export * from './ngx-markdown.component';
-export * from './ngx-markdown.module';
-//# sourceMappingURL=index.js.map
