7728b8bebca0a063c2df4e4730ee3d78f6901bf6
Diabeu <scibor.rudnicki@ster-project.pl>
2017-08-03 16:04:15 +0200

    refactor(MarkdownComponent): remove ./ from templateUrl, remove commented import template, make markdownService private

--- src/ngx-markdown.component.ts
+++ src/ngx-markdown.component.ts
@@ -3,21 +3,22 @@ import { Component, ElementRef, OnInit, Input, ViewChild, ViewEncapsulation } fr
 
 // internal
 import { MarkdownService } from './ngx-markdown.service';
-// import template from ;
 
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'ngx-markdown',
-  templateUrl: './ngx-markdown.component.html'
+  templateUrl: 'ngx-markdown.component.html'
 })
 export class MarkdownComponent implements OnInit {
   @Input() callback: Function;
   @Input() options: any;
   @ViewChild('content', { read: ElementRef }) content: ElementRef;
 
-  constructor(public markdownService: MarkdownService) { }
+  constructor(
+    private markdownService: MarkdownService
+  ) { }
 
   ngOnInit() {
-    this.content.nativeElement.innerHTML = this.markdownService.marked(this.content, this.options, this.callback);
+    this.content.nativeElement.innerHTML = this.markdownService.init(this.content, this.options, this.callback);
   }
 }
