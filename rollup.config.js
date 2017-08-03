1d1f2220f9df7147f8ffa16ec7985fa6aece097b
Diabeu <scibor.rudnicki@ster-project.pl>
2017-08-03 16:34:24 +0200

    refactor(rollup): use rollup-plugin-node-resolve-angular , change plugins nodeResolve

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,6 @@
 import angular from 'rollup-plugin-angular';
 import commonjs from 'rollup-plugin-commonjs';
-import nodeResolve from 'rollup-plugin-node-resolve';
+import nodeResolve from 'rollup-plugin-node-resolve-angular';
 import typescript from 'rollup-plugin-typescript';
 import uglify from 'rollup-plugin-uglify';
 import { minify } from 'uglify-es';
@@ -18,11 +18,11 @@ export default {
     commonjs(),
     nodeResolve({
       // use "es2015" field for ES2015 modules with ES2015 code,
-	    // if possible
+	  // if possible
       es2015: true, // Default: false
 
       // use "module" field for ES2015 modules with ES5 code,
-      // if possible
+	  // if possible
       module: true, // Default: true
 
       // use "jsnext:main" if possible
@@ -34,7 +34,24 @@ export default {
       // – see https://github.com/rollup/rollup-plugin-commonjs
       main: true,  // Default: true
 
-      extensions: [ '.js', '.json' ]
+      // if there's something your bundle requires that you DON'T
+      // want to include, add it to 'skip'. Local and relative imports
+      // can be skipped by giving the full filepath. E.g.,
+      // `path.resolve('src/relative-dependency.js')`
+      skip: [ 'some-big-dependency' ],  // Default: []
+
+      // some package.json files have a `browser` field which
+      // specifies alternative files to load for people bundling
+      // for the browser. If that's you, use this option, otherwise
+      // pkg.browser will be ignored
+      browser: true,  // Default: false
+
+      // not all files you want to resolve are .js files
+      extensions: [ '.js', '.json' ],  // Default: ['.js']
+
+      // whether to prefer built-in modules (e.g. `fs`, `path`) or
+      // local ones with the same names
+      preferBuiltins: false  // Default: true
     }),
     typescript({
       typescript: require('./node_modules/typescript')
