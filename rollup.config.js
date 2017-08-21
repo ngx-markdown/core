92b31610ba4564302ff8e028fabcd14164489729
Diabeu <scibor.rudnicki@ster-project.pl>
2017-08-21 20:42:54 +0200

    chore(rollup.config): sourceMap set to true, add some settings to nodeResolve

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,14 @@ import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import uglify from 'rollup-plugin-uglify';
-import { minify } from 'uglify-es';
+import { minify } from 'uglify-es'; // (uglify plugin) Error: Error transforming bundle with 'uglify' plugin: Unexpected character '`'
 
 export default {
   entry: 'dist/index.js',
   dest: 'dist/bundle.umd.js',
-  exports: 'named',
-  sourceMap: false,
-  format: 'umd',
+  exports: 'named', // – suitable if you're exporting more than one thing
+  sourceMap: true,
+  format: 'umd', // – Universal Module Definition, works as amd, cjs and iife all in one
   moduleName: '@ngx-markdown/core',
   onwarn,
   plugins: [
@@ -34,7 +34,23 @@ export default {
       // – see https://github.com/rollup/rollup-plugin-commonjs
       main: true,  // Default: true
 
-      extensions: [ '.js', '.json' ]
+      extensions: ['.js', '.json'],
+      
+      // whether to prefer built-in modules (e.g. `fs`, `path`) or
+      // local ones with the same names
+      preferBuiltins: true,  // Default: true
+
+      // Lock the module search in this path (like a chroot). Module defined
+      // outside this path will be mark has external
+      jail: '/', // Default: '/'
+
+      // If true, inspect resolved files to check that they are
+      // ES2015 modules
+      modulesOnly: false, // Default: false
+
+      // Any additional options that should be passed through
+      // to node-resolve
+      customResolveOptions: { }
     }),
     typescript({
       typescript: require('./node_modules/typescript')
