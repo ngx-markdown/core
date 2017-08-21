90464c9efd18b17901c0037d67f80281b1313d9e
Diabeu <scibor.rudnicki@ster-project.pl>
2017-08-21 20:44:49 +0200

    refactor(MarkdownComponent):  add ./ to templateUrl

--- src/ngx-markdown.component.ts
+++ src/ngx-markdown.component.ts
@@ -4,19 +4,22 @@ import { Component, ElementRef, OnInit, Input, ViewChild, ViewEncapsulation } fr
 // internal
 import { MarkdownService } from './ngx-markdown.service';
 
+/**
+ * @export
+ * @class MarkdownComponent
+ * @implements {OnInit}
+ */
 @Component({
   encapsulation: ViewEncapsulation.None,
   selector: 'ngx-markdown',
-  templateUrl: 'ngx-markdown.component.html'
+  templateUrl: './ngx-markdown.component.html'
 })
 export class MarkdownComponent implements OnInit {
   @Input() callback: Function;
   @Input() options: any;
   @ViewChild('content', { read: ElementRef }) content: ElementRef;
 
-  constructor(
-    private markdownService: MarkdownService
-  ) { }
+  constructor(private markdownService: MarkdownService) { }
 
   ngOnInit() {
     this.content.nativeElement.innerHTML = this.markdownService.init(this.content, this.options, this.callback);
