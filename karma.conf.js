ab46e8ca3d27b4311c9b0fc064ad0035628a971f
Diabeu <scibor.rudnicki@ster-project.pl>
2017-08-24 16:07:27 +0200

    chore(karma.conf): remove generated, change module name, update nodeResolve config

--- karma.conf.js
+++ karma.conf.js
@@ -1,8 +1,7 @@
 // Karma configuration
-// Generated on Mon Mar 20 2017 15:06:42 GMT+0100 (CET)
 
-const commonjs = require('rollup-plugin-commonjs');
 const angular = require('rollup-plugin-angular');
+const commonjs = require('rollup-plugin-commonjs');
 const nodeResolve = require('rollup-plugin-node-resolve');
 const typescript = require('rollup-plugin-typescript');
 
@@ -47,32 +46,50 @@ module.exports = function(config) {
     rollupPreprocessor: {
       context: 'this',
       // will help to prevent conflicts between different tests entries
-      moduleName: 'ngxmarkdown',
+      moduleName: 'ngx-markdown.core',
       format: 'umd',
-      sourceMap: false,
+      sourceMap: true,
       // rollup settings. See Rollup documentation
       plugins: [
         angular(),
         commonjs(),
         nodeResolve({
-          // use "es2015" field for ES2015 modules with ES2015 code,
-          // if possible
-          es2015: true, // Default: false
-
-          // use "module" field for ES2015 modules with ES5 code,
-          // if possible
+          // use "module" field for ES6 module if possible
           module: true, // Default: true
-
+    
           // use "jsnext:main" if possible
           // – see https://github.com/rollup/rollup/wiki/jsnext:main
           jsnext: true,  // Default: false
-
+    
           // use "main" field or index.js, even if it's not an ES6 module
           // (needs to be converted from CommonJS to ES6
           // – see https://github.com/rollup/rollup-plugin-commonjs
           main: true,  // Default: true
-
-          extensions: [ '.js', '.json' ]
+    
+          // some package.json files have a `browser` field which
+          // specifies alternative files to load for people bundling
+          // for the browser. If that's you, use this option, otherwise
+          // pkg.browser will be ignored
+          browser: true,  // Default: false
+    
+          // not all files you want to resolve are .js files
+          extensions: [ '.js', '.json' ],  // Default: ['.js']
+    
+          // whether to prefer built-in modules (e.g. `fs`, `path`) or
+          // local ones with the same names
+          preferBuiltins: true,  // Default: true
+    
+          // Lock the module search in this path (like a chroot). Module defined
+          // outside this path will be mark has external
+          jail: '/', // Default: '/'
+    
+          // If true, inspect resolved files to check that they are
+          // ES2015 modules
+          modulesOnly: false, // Default: false
+    
+          // Any additional options that should be passed through
+          // to node-resolve
+          customResolveOptions: {}
         }),
         typescript({
           typescript: require('./node_modules/typescript')
