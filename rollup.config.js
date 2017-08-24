c7886b3c839cb8bf3744744d989aa6d051006e0d
Diabeu <scibor.rudnicki@ster-project.pl>
2017-08-24 16:06:36 +0200

    chore(rollup.config): add node-sass, clean-css, html-minifier to  rollup-plugin-angular preprocessor, change to new rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,28 +1,63 @@
+
 import angular from 'rollup-plugin-angular';
+import buble from 'rollup-plugin-buble';
 import commonjs from 'rollup-plugin-commonjs';
 import nodeResolve from 'rollup-plugin-node-resolve';
 import typescript from 'rollup-plugin-typescript';
 import uglify from 'rollup-plugin-uglify';
-import { minify } from 'uglify-es'; // (uglify plugin) Error: Error transforming bundle with 'uglify' plugin: Unexpected character '`'
+import { minify } from 'uglify-es';
+
+// rollup-plugin-angular addons
+import sass from 'node-sass';
+import CleanCSS from 'clean-css';
+import { minify as minifyHtml } from 'html-minifier';
+
+const cssmin = new CleanCSS();
+const htmlminOpts = {
+  caseSensitive: true,
+  collapseWhitespace: true,
+  removeComments: true,
+};
 
 export default {
-  entry: 'dist/index.js',
-  dest: 'dist/bundle.umd.js',
-  exports: 'named', // – suitable if you're exporting more than one thing
-  sourceMap: true,
-  format: 'umd', // – Universal Module Definition, works as amd, cjs and iife all in one
-  moduleName: '@ngx-markdown/core',
+  input: 'dist/index.js',
+  output: {
+    // core output options
+    file: 'dist/bundle.umd.js',    // required
+    format: 'umd',  // required
+    name: 'ngx-markdown.core',
+
+    // advanced output options
+    // paths: ,
+    // banner: ,
+    // footer: ,
+    // intro:,
+    // outro: ,
+    sourcemap: true, // true | inline
+    // sourcemapFile: ,
+    // interop: ,
+
+    // danger zone
+    exports: 'named',
+    // amd: ,
+    // indent: , 
+    // strict: 
+  },
   onwarn,
   plugins: [
-    angular(),
+    angular({
+      preprocessors: {
+        template: template => minifyHtml(template, htmlminOpts),
+        style: scss => {
+          const css = sass.renderSync({ data: scss }).css;
+          return cssmin.minify(css).styles;
+        },
+      }
+    }),
+    buble(),
     commonjs(),
     nodeResolve({
-      // use "es2015" field for ES2015 modules with ES2015 code,
-	    // if possible
-      es2015: true, // Default: false
-
-      // use "module" field for ES2015 modules with ES5 code,
-      // if possible
+      // use "module" field for ES6 module if possible
       module: true, // Default: true
 
       // use "jsnext:main" if possible
@@ -34,8 +69,15 @@ export default {
       // – see https://github.com/rollup/rollup-plugin-commonjs
       main: true,  // Default: true
 
-      extensions: ['.js', '.json'],
-      
+      // some package.json files have a `browser` field which
+      // specifies alternative files to load for people bundling
+      // for the browser. If that's you, use this option, otherwise
+      // pkg.browser will be ignored
+      browser: true,  // Default: false
+
+      // not all files you want to resolve are .js files
+      extensions: [ '.js', '.json' ],  // Default: ['.js']
+
       // whether to prefer built-in modules (e.g. `fs`, `path`) or
       // local ones with the same names
       preferBuiltins: true,  // Default: true
@@ -50,7 +92,7 @@ export default {
 
       // Any additional options that should be passed through
       // to node-resolve
-      customResolveOptions: { }
+      customResolveOptions: {}
     }),
     typescript({
       typescript: require('./node_modules/typescript')
