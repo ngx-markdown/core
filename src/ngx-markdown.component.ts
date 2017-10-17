b5404043b572ae059623199c84e134a9ff5a9f36
Diabeu <scibor.rudnicki@ster-project.pl>
2017-10-17 11:50:08 +0200

    refactor(MarkdownComponent): add OnPush change detection, implements AfterViewInit DoCheck OnChanges instead of OnInit

--- src/ngx-markdown.component.ts
+++ src/ngx-markdown.component.ts
@@ -1,27 +1,106 @@
 // external
-import { Component, ElementRef, OnInit, Input, ViewChild, ViewEncapsulation } from '@angular/core';
+import {
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  ContentChild,
+  ChangeDetectorRef,
+  ChangeDetectionStrategy,
+  DoCheck,
+  ElementRef,
+  Input,
+  KeyValueChangeRecord,
+  KeyValueDiffers,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+  ViewChild,
+  ViewEncapsulation
+} from '@angular/core';
+import * as _ from 'lodash-es';
 
 // internal
+import { MarkdownHoodClass } from './ngx-markdown.class';
 import { MarkdownService } from './ngx-markdown.service';
+
+function instanceOf<T>(object: any): object is T {
+  return <T>object !== undefined;
+}
 
 /**
  * @export
  * @class MarkdownComponent
- * @implements {OnInit}
+ * @extends {MarkdownHoodClass}
+ * @implements {AfterViewChecked}
+ * @implements {AfterViewInit}
+ * @implements {OnChanges}
  */
 @Component({
+  changeDetection: ChangeDetectionStrategy.OnPush,
   encapsulation: ViewEncapsulation.None,
   selector: 'ngx-markdown',
   templateUrl: './ngx-markdown.component.html'
 })
-export class MarkdownComponent implements OnInit {
-  @Input() callback: any;
-  @Input() options: marked.MarkedOptions;
-  @ViewChild('content', { read: ElementRef }) content: ElementRef;
+export
+  class MarkdownComponent
+  extends MarkdownHoodClass
+  implements
+  AfterViewInit,
+  DoCheck,
+  OnChanges {
 
-  constructor(private markdownService: MarkdownService) { }
+  /**
+   * Creates an instance of MarkdownComponent.
+   * @param {MarkdownService} markdownService
+   * @param {ChangeDetectorRef} changeDetectorRef
+   * @param {KeyValueDiffers} keyValueDiffers
+   * @memberof MarkdownComponent
+   */
+  constructor(
+    public markdownService: MarkdownService,
+    protected changeDetectorRef: ChangeDetectorRef,
+    protected keyValueDiffers: KeyValueDiffers
+  ) {
+    super(markdownService, changeDetectorRef, keyValueDiffers);
+  }
 
-  ngOnInit() {
-    this.content.nativeElement.innerHTML = this.markdownService.init(this.content, this.options, this.callback);
+  /**
+   * First init performed on `ng-content` property.
+   * @memberof MarkdownComponent
+   */
+  ngAfterViewInit(): void {
+    if (!this.string) {
+      // Store `ng-content` to work on it later.
+      this.storeNgContent();
+      // Perform init on stored `ngContent`.
+      this.change = true;
+    }
+  }
+
+  /**
+   * @memberof MarkdownComponent
+   */
+  ngDoCheck(): void {
+    let changes = null;
+    changes = this.differ.options.diff(this.options);
+    if (changes) {
+      // Detect changes in `options` property.
+      changes.forEachChangedItem((record: KeyValueChangeRecord<string, any>) => {
+        if (this.options) {
+          this.options[record.key] = record.currentValue;
+          this.stringMarked = undefined;
+          this.change = true;
+        }
+      });
+    }
+  }
+
+  /**
+   * Detect changes on properties `string` and `callback`.
+   * @param {SimpleChanges} changes
+   * @memberof MarkdownComponent
+   */
+  ngOnChanges(changes: SimpleChanges): void {
+    this.onChanges(['callback', 'string'], changes);
   }
 }
