eac2b3c35bb663486a5235348e3f612d068e4466
Diabeu <scibor.rudnicki@ster-project.pl>
2017-10-17 11:43:55 +0200

    test(index): add more timeout_interval for jasmine, remove core-js

--- test/index.ts
+++ test/index.ts
@@ -1,7 +1,4 @@
 import 'reflect-metadata';
-import 'core-js/es6';
-import 'core-js/es7/reflect';
-
 import 'zone.js/dist/zone';
 import 'zone.js/dist/long-stack-trace-zone';
 import 'zone.js/dist/proxy';
@@ -9,8 +6,6 @@ import 'zone.js/dist/sync-test';
 import 'zone.js/dist/jasmine-patch';
 import 'zone.js/dist/async-test';
 import 'zone.js/dist/fake-async-test';
-
-// RxJS
 import 'rxjs/Rx';
 
 import * as browser from '@angular/platform-browser-dynamic/testing';
@@ -20,5 +15,6 @@ testing.TestBed.initTestEnvironment(
   browser.BrowserDynamicTestingModule,
   browser.platformBrowserDynamicTesting()
 );
+
 Error.stackTraceLimit = Infinity;
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 2000;
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
